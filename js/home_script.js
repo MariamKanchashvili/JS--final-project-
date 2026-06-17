a9a54ff07d4ffff73ed394f127211e667a3014ac
MariamKanchashvili <mariam.kanchashvili748@eab.tsu.edu.ge>
2026-06-17 18:36:49 +0400

    alert update

--- js/home_script.js
+++ js/home_script.js
@@ -278,6 +278,7 @@ async function addToCart(productId) {
   const token = sessionStorage.getItem("accessToken");
   if (!token) {
     alert("Please sign in first");
+      window.location.href = "logIn_index.html";
     return;
   }
 
--- js/text_shop_Signed.js
+++ js/text_shop_Signed.js
@@ -298,6 +298,7 @@ searchInputHeader.addEventListener("input", async (e) => {
   const token = sessionStorage.getItem("accessToken");
   if (!token) {
     alert("Please sign in first");
+      window.location.href = "logIn_index.html";
     return;
   }
 
