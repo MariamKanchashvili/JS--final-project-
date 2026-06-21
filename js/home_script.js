6e54df425793c6c122a3bb610b77f58cfdbdc6e7
MariamKanchashvili <mariam.kanchashvili748@eab.tsu.edu.ge>
2026-06-22 00:29:38 +0400

    ready for presentation

--- js/home_script.js
+++ js/home_script.js
@@ -1,9 +1,11 @@
+// ==========================
+// CAROUSEL
+// ==========================
 const track = document.querySelector(".products__track");
 
 let currentSlide = 0;
 const visibleCards = 5;
 const cardWidth = 270;
-
 let autoSlideInterval;
 
 // ==========================
@@ -19,10 +21,10 @@ async function getProducts() {
 
         renderProducts(data.products);
 
-        initAutoCarousel(); // autoplay აქ იწყება
+        initAutoCarousel();
 
     } catch (error) {
-        console.log("შეცდომაა",error);
+        console.log("შეცდომაა", error);
     }
 }
 
@@ -33,96 +35,44 @@ function renderProducts(products) {
 
     track.innerHTML = products.map(product => {
 
-        // ფასდაკლება
-        const hasNoDiscount =
-            product.price.discountPercentage === 0;
+        const hasNoDiscount = product.price.discountPercentage === 0;
+        const hideClass = hasNoDiscount ? "hidden" : "";
 
-        const hideClass =
-            hasNoDiscount ? "hidden" : "";
-
-        // მარაგი
-        const isOutOfStock =
-            product.stock <= 0;
-
-        const stockText =
-            isOutOfStock
-                ? "Out of stock"
-                : `In Stock: ${product.stock}`;
-
-        const stockClass =
-            isOutOfStock
-                ? "no-stock"
-                : "in-stock";
-
-        const isButtonDisabled =
-            isOutOfStock
-                ? "disabled"
-                : "";
+        const isOutOfStock = product.stock <= 0;
+        const stockText = isOutOfStock ? "Out of stock" : `In Stock: ${product.stock}`;
+        const stockClass = isOutOfStock ? "no-stock" : "in-stock";
+        const isButtonDisabled = isOutOfStock ? "disabled" : "";
 
         return `
         <div class="products__card">
-
-            <img
-                src="${product.thumbnail}"
-                class="products__image"
-                referrerpolicy="no-referrer"
-            >
-
-            <h3>
-                ${product.title}
-            </h3>
-
-            <p class="products__card-brand">
-                ${product.brand.toUpperCase()}
-            </p>
-
-            <p>
-                ${product.category.name}
-            </p>
-
-            <p class="products__stock ${stockClass}">
-                ${stockText}
-            </p>
-
-            <p>
-                ${"⭐".repeat(
-                    Math.round(product.rating)
-                )}
-            </p>
+            <img src="${product.thumbnail}" class="products__image" referrerpolicy="no-referrer">
+            <h3>${product.title}</h3>
+            <p class="products__card-brand">${product.brand.toUpperCase()}</p>
+            <p>${product.category.name}</p>
+            <p class="products__stock ${stockClass}">${stockText}</p>
+            <p>${"⭐".repeat(Math.round(product.rating))}</p>
 
             <div class="products__price">
-
                 <span class="products__price-old ${hideClass}">
-                    ${product.price.beforeDiscount}
-                    ${product.price.currency}
+                    ${product.price.beforeDiscount} ${product.price.currency}
                 </span>
-
                 <span class="products__price-current">
-                    ${product.price.current}
-                    ${product.price.currency}
+                    ${product.price.current} ${product.price.currency}
                 </span>
-
                 <span class="discount ${hideClass}">
                     -${product.price.discountPercentage}%
                 </span>
-
             </div>
 
-            <button
-                class="products__cart"
-                ${isButtonDisabled}
-                onclick="addToCart('${product._id}') "
-            >
-                ${
-                    isOutOfStock
-                        ? "Unavailable"
-                        : "Add To Cart"
-                }
+            <button class="products__cart" ${isButtonDisabled}
+                onclick="addToCart('${product._id}')">
+                ${isOutOfStock ? "Unavailable" : "Add To Cart"}
             </button>
-<button class="viewDetailsBtn"
-onclick="window.location.href='product-Id.html?id=${product._id}'">
-View Details
-</button>    
+
+            <button class="viewDetailsBtn"
+                onclick="window.location.href='product-Id.html?id=${product._id}'">
+                View Details
+            </button>
         </div>
         `;
 
@@ -130,18 +80,14 @@ View Details
 }
 
 // ==========================
-// AUTO CAROUSEL 
+// AUTO CAROUSEL
 // ==========================
 function initAutoCarousel() {
 
-    const totalCards =
-        document.querySelectorAll(".products__card").length;
-
-    const maxSlide =
-        Math.max(totalCards - visibleCards, 0);
+    const totalCards = document.querySelectorAll(".products__card").length;
+    const maxSlide = Math.max(totalCards - visibleCards, 0);
 
     currentSlide = 0;
-
     track.style.transform = "translateX(0px)";
 
     if (autoSlideInterval) {
@@ -149,163 +95,17 @@ function initAutoCarousel() {
     }
 
     autoSlideInterval = setInterval(() => {
-
         if (currentSlide >= maxSlide) {
             currentSlide = 0;
         } else {
             currentSlide++;
         }
-
-        track.style.transform =
-            `translateX(-${currentSlide * cardWidth}px)`;
-
+        track.style.transform = `translateX(-${currentSlide * cardWidth}px)`;
     }, 2500);
 }
-
 
 // ==========================
 // START
 // ==========================
 getProducts();
 
-// search
-
-const searchInput=document.getElementById("searchInput");
-const searchBtn=document.getElementById("searchBtn");
-const searchList=document.getElementById("searchList");
-
-
-async function searchProducts(){
-
-    try {
-
-        const keyword=searchInput.value;
-
-        const url=`https://api.everrest.educata.dev/shop/products/search?keywords=${keyword}`;
-
-        const response=await fetch(url);
-
-        const data=await response.json();
-
-        renderProducts(data.products);
-
-    } catch(error){
-
-        alert("დაფიქსირდა შეცდომა ძებნისას");
-        console.log("შეცდომაა ძებნისას",error);
-
-    }
-}
-
-searchBtn.addEventListener("click",()=>{
-
-    searchProducts();
-
-});
-
-console.log("listener-მდე მოვედი");
-
-
-
-// dropdown  ით ძებნის შედეგების გამოტანა 
-searchInput.addEventListener("input", async () => {
-
-    const keyword = searchInput.value.trim();
-
-    if(keyword.length < 2){
-        searchList.innerHTML = "";
-        searchList.style.display = "none";
-        return;
-    }
-
-    const response = await fetch(
-        `https://api.everrest.educata.dev/shop/products/search?keywords=${keyword}`
-    );
-
-    const data = await response.json();
-
-    searchList.innerHTML = "";
-
-    if(data.products.length === 0){
-        searchList.innerHTML = "<p>Product not found</p>";
-        searchList.style.display = "block";
-        return;
-    }
-
-    data.products.forEach(product => {
-
-        const item = document.createElement("div");
-
-        item.classList.add("search-item");
-
-        item.innerHTML = `
-            <img src="${product.thumbnail}" width="50">
-            <span>${product.title}</span>
-        `;
- item.addEventListener("click", () => {
-    window.location.href = `http://127.0.0.1:5500/html/product-Id.html?id=${product._id}`;
-});
-
-        searchList.appendChild(item);
-    });
-
-    searchList.style.display = "block";
-
-});
-
-
-async function addToCart(productId) {
-  const token = sessionStorage.getItem("accessToken");
-  if (!token) {
-    alert("Please sign in first");
-      window.location.href = "logIn_index.html";
-    return;
-  }
-
-  const cart = await getCartData();
-
-  if (!cart) {
-    await sendCartRequest("POST", productId, 1);
-    return;
-  }
-
-  const existingProduct = cart.products.find((p) => p.productId === productId);
-  const newQuantity = existingProduct ? existingProduct.quantity + 1 : 1;
-  const method = cart.products.length > 0 ? "PATCH" : "POST";
-
-  await sendCartRequest(method, productId, newQuantity);
-}
-const globalCartCount = document.querySelector(".global-cart-count");
-
-function updateGlobalBadge(quantity) {
-  if (globalCartCount) {
-    globalCartCount.textContent = quantity;
-  }
-}
-
-async function loadCartBadge() {
-  const token = sessionStorage.getItem("accessToken");
-
-  if (!token) return;
-
-  try {
-    const response = await fetch(
-      "https://api.everrest.educata.dev/shop/cart",
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      }
-    );
-
-    const data = await response.json();
-
-    if (response.ok) {
-      updateGlobalBadge(data.total.quantity);
-    }
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-window.addEventListener("DOMContentLoaded", loadCartBadge);
